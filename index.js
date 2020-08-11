a58c5128d42d2901ae65f59239b14997c834facd
CStank89 <C.Stank89@gmail.com>
2020-08-11 17:38:58 -0400

    Finished up the dog feeder, working on finishing the rock paper scissors

--- index.js
+++ index.js
@@ -47,8 +47,8 @@
 
 // multiple age by 7
 
-// let age = '31'
-// let dogAge ='7'
+// let age = 31
+// let dogAge = 7
 
 // function multiply(age  , dogAge){
 //     return age * dogAge;
@@ -58,23 +58,66 @@
 
 
 /************************************************************** Task 3 **************************************************************/
-//Dog feeder 
-//takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
+// Dog feeder 
+// takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 
-//feeding requirements
+// feeding requirements
+
 // adult dogs at least 1 year 
-// up to 5 lbs - 5% of their body weight
-// 6 - 10 lbs - 4% of their body weight 
-// 11 - 15 lbs - 3% of their body weight 
-// > 15lbs - 2% of their body weight 
+    // up to 5 lbs - 5% of their body weight
+    // 6 - 10 lbs - 4% of their body weight 
+    // 11 - 15 lbs - 3% of their body weight 
+    // > 15lbs - 2% of their body weight 
 
-// Puppies less than 1 year
-// 2 - 4 months 10% of their body weight
-// 4 - 7 months 5% of their body weight 
-// 7 - 12 months 4% of their body weight
+ // Puppies less than 1 year
+    // 2 - 4 months 10% of their body weight
+    // 4 - 7 months 5% of their body weight 
+    // 7 - 12 months 4% of their body weight
 
 
-// let weight =''
+function dogfeeder(weight , age){
+    if (age >= 1){
+        if (weight <= 5){
+        return weight*.05;
+        }
+        else if(weight <= 10){
+        return weight*.04;
+        }
+        else if(weight <= 15){
+        return weight*.03;
+        }
+        else{
+        return weight*.02;
+        }
+    }
+    else{
+        if (age <= 4/12){
+        return weight*.10;
+        }
+        else if(age <= 7/12){
+        return weight*.05;
+        }
+        else{
+        return weight*.04;
+        }
+
+    }
+
+}
+    console.log (dogfeeder (4 , 3/12 ));
+
+
+//     if (year >= 1 && weight <= 5){
+//         console.log(weight*.05)
+//     }
+//     if (year >= 1 && weight >= 6 && weight <= 10){
+//         console.log(weight*.04)
+//     }
+//     if (year >= 1 && weight >= 11 && weight <=15){
+//         console.log(weight*.03)
+//     }
+
+
 
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
@@ -91,31 +134,66 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-function choices(){
-let random = math.random()
-if (random < .3){
-    console.log('rock')
-}
-if (random > .3 && random < .6){
-    console.log('paper')
-}
-if (random > .6){
-    console.log('scissors')
-}
-  
+ // function choices(){
+    // function choices(selection) {
+    // let random = Math.random();
+    //     console.log(random);
 
+    // if (random <= 0.3) {
+    //     console.log('Computer chooses rock.');
+    // if (selection === 'rock') {
+    //     console.log('Games ends in a draw');
+    //     }
+    //     if (selection === 'paper') {
+    //     console.log('Paper covers rock! You Win!');
+    //     }
+    //     if (selection === 'scissors') {
+    //     console.log('Rock beats scissors! You Lose!');
+    //     }
+    //     }
+    //     if (random > 0.3 && random <= 0.6) {
+    //     console.log('Computer chooses paper');
+    //     if (selection === 'paper'){
+    //     console.log('Game ends in a draw');
+    //     }
+    //     if (selection === 'rock') {
+    //     console.log('Paper cover rock! You Lose');
+    //     }
+    //     if (selection === 'scissors'){
+    //     console.log('Scissors beats paper! You win')
+    //     }  
+    //     }
+
+    //     if (random > 0.6) {
+    //     console.log("scissors");
+        
+    //     }
+    //     }
+    //     console.log (choices('rock'));
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
 
+// let factor = 0.621371
+// let kilometers = (10);
 
+// function multiply(kilometers , factor){
+//     return kilometers * factor;
+
+// }
+// console.log(kilometers*factor + ' miles' );
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
+// let factor = 30.48
+// let feet =(2);
 
-
+// function multiply(feet , factor){
+//     return feet * factor;
+// }
+// console.log(feet*factor + ' centimeters');
 
 
 /************************************************************** Task 6 **************************************************************/
@@ -123,7 +201,9 @@ if (random > .6){
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+// // for (let i = 0, i < 99; i--){
 
+// }
 
 
 
